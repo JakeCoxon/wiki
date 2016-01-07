6d107dd91dff4bf569891ec17354a2db00f68a3d
Jake Coxon <jake.coxon@gmail.com>
2016-01-07 18:17:55 +0000

    typo

--- src/js/views/tree-view.js
+++ src/js/views/tree-view.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import * as actions from '../actions-creators'
+import * as actions from '../action-creators'
 
 const TreeViewUnderlying = React.createClass({
 
