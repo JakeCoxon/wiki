f9c1ebe59c008a0163a090d9ddc17f769918a0a6
Jake Coxon <jake.coxon@gmail.com>
2016-01-07 16:43:19 +0000

    Update 2016-01-07T16:43:16.640Z

--- src/js/util/mergeClasses.js
+++
@@ -1,3 +0,0 @@
-export default function mergeClasses() {
-    return Array.prototype.slice.call(arguments).filter(x => !!x).join(" ")
-}
--- src/js/util/removeAt.js
+++
@@ -1,7 +0,0 @@
-export default function removeAt(array, findIndex) {
-    const arr = new Array()
-    array.forEach((item, index) => {
-        if (index !== findIndex) arr.push(item);
-    });
-    return arr;
-}
